0d6d9182573c089ac60000e5e081021b37241789
Christopher Keers <cdkeers@gmail.com>
2021-01-01 22:42:43 -0700

    major improvements
    Added Highlight JS in the stem.js file so it is auto loaded on
    every page just like Katex and Mermaid. Major CSS tweaks. Completed
    the basic style guide page and added support for images: image
    alignment and image captions. TODO: Fix list styles, they are all
    broken now.

--- js/stem.js
+++ js/stem.js
@@ -30,27 +30,60 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
         count--;
     }
 
-    // Load the Katex stylesheet into the current page.
+    // Load the Highlight JS stylesheet into the current page.
     var tag  = document.createElement( 'LINK' );
+    tag.rel  = 'stylesheet';
+    tag.href = relPath + 'css/highlight.min.css';
+    document.head.appendChild( tag );
+
+    // Load the Highlight JS script into the current page.
+    tag      = document.createElement( 'SCRIPT' );
+    tag.type = 'text/javascript';
+    tag.src  = relPath + 'js/highlight.min.js';
+    document.head.appendChild( tag );
+
+    // Load the Katex stylesheet into the current page.
+    tag  = document.createElement( 'LINK' );
     tag.rel  = 'stylesheet';
     tag.href = relPath + 'css/katex.min.css';
     document.head.appendChild( tag );
 
     // Load the Katex script into the current page.
-    var tag  = document.createElement( 'SCRIPT' );
+    tag      = document.createElement( 'SCRIPT' );
     tag.type = 'text/javascript';
     tag.src  = relPath + 'js/katex.min.js';
     document.head.appendChild( tag );
 
     // Load the Mermaid script into the current page.
-    var tag  = document.createElement( 'SCRIPT' );
+    tag      = document.createElement( 'SCRIPT' );
     tag.type = 'text/javascript';
     tag.src  = relPath + 'js/mermaid.min.js';
     document.head.appendChild( tag );
 
     // Once the scripts have been loaded call the functions to initialize them.
+    loadHighlight( 0 );
     loadMermaid( 0 );
     loadKatex( 0 );
+
+    function loadHighlight( count ) {
+
+        if ( count > 30 ) {
+            return;
+        }
+
+        if ( typeof mermaid !== 'undefined' ) {
+
+            // Initialize Highlight.js to format all code examples on the page.
+            var tag  = document.createElement( 'SCRIPT' );
+            tag.type = 'text/javascript';
+            tag.innerHTML = 'hljs.initHighlighting();';
+            document.head.appendChild( tag );
+
+        } else {
+            setTimeout( loadHighlight.bind( null, count++ ), 100 );
+        }
+
+    }
 
     function loadMermaid( count ) {
 
