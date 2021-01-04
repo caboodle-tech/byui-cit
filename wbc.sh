3a08051e7a01ff4b3ad231d1ba83951b7abe6df6
Christopher Keers <cdkeers@gmail.com>
2021-01-03 20:55:45 -0700

    updated workflow

--- wbc.sh
+++
@@ -1,9 +0,0 @@
-check=git branch | grep website
-if [ -z '' ]
-then
-  echo 'Website branch does not exist, creating now'
-  git checkout -b website
-  git push --set-upstream origin website
-else
-  echo 'Website branch exists'
-if [ -z '$check' ]
